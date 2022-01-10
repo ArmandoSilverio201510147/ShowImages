5317a2b5320d4299a37a209570fef68235f6bc2e
Armando <ve09829@innovaccion.mx>
2022-01-10 15:37:47 -0600

    This is an example showing three images whit its tag name

---
+++ script.js
@@ -0,0 +1,9 @@
+document.write("<h1> This is a BMW</h1>");
+/*var qrLastSupper = document.createElement('img');
+qrLastSupper.src = "images/bmw.jfif";
+document.body.appendChild(qrLastSupper);
+
+document.write("<h1> This is a Maserati</h1>");
+var next = document.createElement('img');
+next.src = "images/maserati.jfif";
+document.body.appendChild(next);*/
